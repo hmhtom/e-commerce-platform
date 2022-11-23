a5bcff27af77573c950eb2e1f5974ccac6d1358d
hmhtom <hmhtom@gmail.com>
2022-11-22 23:29:33 -0500

    initial commit

---
+++ client/build/precache-manifest.60a8afaf211edcfe539196515eeb3ef5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "957506737fc05bd1c67dbb415e7cf0bb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8f23acac8e3111e1444f",
+    "url": "/static/css/main.a64bd9e7.chunk.css"
+  },
+  {
+    "revision": "09c209b961932cbb71f5",
+    "url": "/static/js/2.6c29a1af.chunk.js"
+  },
+  {
+    "revision": "6865f50f3618af47c938d1e1cd5951cf",
+    "url": "/static/js/2.6c29a1af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f23acac8e3111e1444f",
+    "url": "/static/js/main.17945f24.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.17945f24.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d52f44d6fb82598fed4",
+    "url": "/static/js/runtime-main.c62b8046.js"
+  },
+  {
+    "revision": "ab4978553bd68ca15e503a0da7fa604d",
+    "url": "/static/media/spinner.ab497855.gif"
+  }
+]);
