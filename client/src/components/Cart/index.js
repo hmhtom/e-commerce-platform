32c08b70675fd2b1c25022ff8e8a0406f2939ebd
hmhtom <hmhtom@gmail.com>
2022-11-22 23:30:59 -0500

    replace useContext with useSelect/useDispatch

---
+++ client/src/components/Cart/index.js
@@ -0,0 +1,101 @@
+import React, { useEffect } from "react";
+import { loadStripe } from "@stripe/stripe-js";
+import { useLazyQuery } from "@apollo/client";
+import { QUERY_CHECKOUT } from "../../utils/queries";
+import { idbPromise } from "../../utils/helpers";
+import CartItem from "../CartItem";
+import Auth from "../../utils/auth";
+
+import { useSelector, useDispatch } from "react-redux";
+import { TOGGLE_CART, ADD_MULTIPLE_TO_CART } from "../../utils/storeSlice";
+
+import "./style.css";
+
+const stripePromise = loadStripe("pk_test_TYooMQauvdEDq54NiTphI7jx");
+
+const Cart = () => {
+  const [getCheckout, { data }] = useLazyQuery(QUERY_CHECKOUT);
+  const cart = useSelector((state) => state.store.cart);
+  const cartOpen = useSelector((state) => state.store.cartOpen);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    if (data) {
+      stripePromise.then((res) => {
+        res.redirectToCheckout({ sessionId: data.checkout.session });
+      });
+    }
+  }, [data]);
+
+  useEffect(() => {
+    async function getCart() {
+      const storedCart = await idbPromise("cart", "get");
+      console.log(storedCart);
+      dispatch({ type: ADD_MULTIPLE_TO_CART, products: storedCart });
+    }
+    if (!cart.length) {
+      getCart();
+    }
+  }, [cart.length, cartOpen]);
+
+  function calculateTotal() {
+    let sum = 0;
+    cart.forEach((item) => {
+      sum += item.price * item.purchaseQuantity;
+    });
+    return sum.toFixed(2);
+  }
+
+  function submitCheckout() {
+    const productIds = [];
+    cart.forEach((item) => {
+      for (let i = 0; i < item.purchaseQuantity; i++) {
+        productIds.push(item._id);
+      }
+    });
+    getCheckout({
+      variables: { products: productIds },
+    });
+  }
+
+  if (!cartOpen) {
+    return (
+      <div className="cart-closed" onClick={() => dispatch(TOGGLE_CART())}>
+        <span role="img" aria-label="trash">
+          🛒
+        </span>
+      </div>
+    );
+  }
+  return (
+    <div className="cart">
+      <div className="close" onClick={() => dispatch(TOGGLE_CART())}>
+        [close]
+      </div>
+      <h2>Shopping Cart</h2>
+      {cart.length ? (
+        <div>
+          {cart.map((item) => (
+            <CartItem key={item._id} item={item} />
+          ))}
+          <div className="flex-row space-between">
+            <strong>Total: ${calculateTotal()}</strong>
+            {Auth.loggedIn() ? (
+              <button onClick={submitCheckout}>Checkout</button>
+            ) : (
+              <span>(log in to check out)</span>
+            )}
+          </div>
+        </div>
+      ) : (
+        <h3>
+          <span role="img" aria-label="shocked">
+            😱
+          </span>
+          You haven't added anything to your cart yet!
+        </h3>
+      )}
+    </div>
+  );
+};
+
+export default Cart;
